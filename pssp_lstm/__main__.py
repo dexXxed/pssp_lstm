c2f81fd67f1e7938ae1aaf1f61653c27edf851a4
dexxxed <denis22052000@gmail.com>
2020-03-11 22:08:33 +0200

    __main__.py готово

--- pssp_lstm/__main__.py
+++ pssp_lstm/__main__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """Driver для обучения и тренировки моделей."""
 import argparse as ap
 from pathlib import Path
@@ -6,11 +5,10 @@ from .train import train
 from .evaluate import evaluate
 from .hparams import HPARAMS
 
+
 def main():
-
-
-    #  Определите основной аргумент парсера
-    parser = ap.ArgumentParser(prog="pssp_lstm", description="Train and run models.",
+    # Определим основной аргумент парсера
+    parser = ap.ArgumentParser(prog="pssp_lstm", description="Train and run models",
                                argument_default=ap.SUPPRESS)
 
     subparsers = parser.add_subparsers(title='subcommands')
@@ -41,9 +39,8 @@ def main():
 
     args = parser.parse_args()
 
-
     if args.entry == "train":
-        #  запустить тренировку
+        #  запустим на обучение
         HPARAMS.logging = args.logging
 
         logpath = Path(args.logdir)
@@ -54,12 +51,11 @@ def main():
         train(HPARAMS)
 
     elif args.entry == "evaluate":
-        # оцените обученную сеть по тестовым данным
+        # оценим обученную сеть по тестовым данным
         HPARAMS.valid_file = str(Path(args.datadir, "cpdb_513.tfrecords").absolute())
         HPARAMS.model_ckpt = str(Path(args.ckpt).absolute())
 
         evaluate(HPARAMS)
-
 
 
 if __name__ == "__main__":
